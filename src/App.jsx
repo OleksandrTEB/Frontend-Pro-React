d8af1d557d466422942fefea82d28d84545818d6
Oleksandr Shvab <fiuhad@gmail.com>
2026-04-22 10:53:57 +0200

    Lesson25-Homework1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import Header from './components/Header/Header.jsx';
+import Main from "./components/Main/Main.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+
+import './App.css';
+
 function App() {
-  return (
-      <div></div>
-  )
+    return (
+        <div className="app-container">
+            <Header />
+
+            <Main />
+
+            <Footer />
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <main>
+                Main
+            </main>
+        )
+    }
+}
+
+export default Main;
